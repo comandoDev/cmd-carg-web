03ab0055634a7a9a24744540318931f0317e2890
rodolfosouzamenezes <rodolfosouzaofc@gmail.com>
2024-06-12 16:46:00 -0300

    refactor: rename file saheader to header

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import { Header } from '@/components/saheader'
+import { Header } from '@/components/header'
 
 export function AppLayout() {
   return (
